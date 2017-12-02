fb3a2928931ec1e3987072588025b13d3a882355
barkep <bartekkepka6@gmail.com>
2017-12-02 23:51:42 +0100

    ver 0.1

--- src/apka_kurs/Library.java
+++ src/apka_kurs/Library.java
@@ -1,5 +1,26 @@
 package apka_kurs;
 
 public class Library {
+	
+	public static void main(String [] args) {
+		
+		final String appName = "Biblioteka v0.1";
+		
+		String tytul="W pustyni i w puszczy";
+		String autor="Henryk Sienkiewicz:";
+		int rokWydania=2010;
+		int iloscStron=296;
+		String wydawnictwo="Greg";
+		String isbn="9788373271890";
+		
+		System.out.println(appName);
+        System.out.println("Ksi¹¿ki dostêpne w bibliotece:");
+        System.out.println(tytul);
+        System.out.println(autor);
+        System.out.println(rokWydania);
+        System.out.println(iloscStron);
+        System.out.println(wydawnictwo);
+        System.out.println(isbn);
+	}
 
 }
