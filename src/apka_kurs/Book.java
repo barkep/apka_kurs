b49a1f6c4f2266c010bedf09feff3fd707f680e6
barkep <bartekkepka6@gmail.com>
2017-12-03 00:54:47 +0100

    ver 0.3

--- src/apka_kurs/Book.java
+++ src/apka_kurs/Book.java
@@ -8,4 +8,18 @@ public class Book {
 	int iloscStron;
 	String wydawnictwo;
 	String isbn;
+
+	Book(String tytul, String autor, int rokWydania, int iloscStron, String wydawnictwo, String isbn) {
+		this.tytul = tytul;
+		this.autor = autor;
+		this.rokWydania = rokWydania;
+		this.iloscStron = iloscStron;
+		this.wydawnictwo = wydawnictwo;
+		this.isbn = isbn;
+	}
+
+	void printInfo() {
+		System.out.println(
+				tytul + "; " + autor + "; " + rokWydania + "; " + iloscStron + "; " + wydawnictwo + "; " + isbn);
+	}
 }
--- src/apka_kurs/Library.java
+++ src/apka_kurs/Library.java
@@ -4,25 +4,12 @@ public class Library {
 	
 	public static void main(String [] args) {
 		
-		final String appName = "Biblioteka v0.2";
+		final String appName = "Biblioteka v0.3";
 		
-		Book book1=new Book();
-		
-		book1.tytul="W pustyni i w puszczy";
-		book1.autor="Henryk Sienkiewicz:";
-		book1.rokWydania=2010;
-		book1.iloscStron=296;
-		book1.wydawnictwo="Greg";
-		book1.isbn="9788373271890";
-		
+		Book book1=new Book("W pustyni i w puszczy","Henryk Sienkiewicz:",2010,296,"Greg","9788373271890");
+
 		System.out.println(appName);
-        System.out.println("Ksi¹¿ki dostêpne w bibliotece:");
-        System.out.println(book1.tytul);
-        System.out.println(book1.autor);
-        System.out.println(book1.rokWydania);
-        System.out.println(book1.iloscStron);
-        System.out.println(book1.wydawnictwo);
-        System.out.println(book1.isbn);
-	}
+		book1.printInfo();
+        }
 
 }
