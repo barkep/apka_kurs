fb256243a56b93a6cde00fcbad2665a6e3947b49
barkep <bartekkepka6@gmail.com>
2017-12-16 03:06:15 +0100

    ver 0.5

--- src/apka_kurs/Library.java
+++ src/apka_kurs/Library.java
@@ -1,10 +1,12 @@
 package apka_kurs;
 
+import data.Book;
+
 public class Library {
 
 	public static void main(String[] args) {
 
-		final String appName = "Biblioteka v0.4";
+		final String appName = "Biblioteka v0.5";
 
 		Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz:", 2010, 296, "Greg", "9788373271890");
 		Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
@@ -12,6 +14,7 @@ public class Library {
 		Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
 				2008, 851, "McGraw-Hill Osborne Media", "9780071591065");
 		System.out.println(appName);
+		System.out.println("Ksi¹¿ki w bibliotece: ");
 		book1.printInfo();
 		book2.printInfo();
 		book3.printInfo();
---
+++ src/data/Book.java
@@ -0,0 +1,81 @@
+package data;
+
+public class Book {
+
+	private String tytul;
+	private String autor;
+	private int rokWydania;
+	private int iloscStron;
+	private String wydawnictwo;
+	private String isbn;
+	
+	public Book(String tytul, String autor, int rokWydania, int iloscStron, String wydawnictwo, String isbn) {
+		this.tytul=tytul;
+		this.autor=autor;
+		this.rokWydania=rokWydania;
+		this.iloscStron=iloscStron;
+		this.wydawnictwo=wydawnictwo;
+		this.isbn=isbn;
+	}
+	
+	public Book(Book book) {
+		this(book.tytul,book.autor,book.rokWydania,book.iloscStron,book.wydawnictwo,book.isbn);
+	}
+	
+	//gettery i settery
+	
+	public String getTytul() {
+		return tytul;
+	}
+
+	public void setTytul(String tytul) {
+		this.tytul = tytul;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getRokWydania() {
+		return rokWydania;
+	}
+
+	public void setRokWydania(int rokWydania) {
+		this.rokWydania = rokWydania;
+	}
+
+	public int getIloscStron() {
+		return iloscStron;
+	}
+
+	public void setIloscStron(int iloscStron) {
+		this.iloscStron = iloscStron;
+	}
+
+	public String getWydawnictwo() {
+		return wydawnictwo;
+	}
+
+	public void setWydawnictwo(String wydawnictwo) {
+		this.wydawnictwo = wydawnictwo;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	
+	public void printInfo() {
+		System.out.println(
+				tytul + "; " + autor + "; " + rokWydania + "; " + iloscStron + "; " + wydawnictwo + "; " + isbn);
+	}
+
+}
