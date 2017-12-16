273cccd685a9bdb16d31d19299ad7dfc3f66b781
barkep <bartekkepka6@gmail.com>
2017-12-16 03:17:26 +0100

    ver 0.6

--- src/apka_kurs/Library.java
+++ src/apka_kurs/Library.java
@@ -6,18 +6,19 @@ public class Library {
 
 	public static void main(String[] args) {
 
-		final String appName = "Biblioteka v0.5";
-
-		Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz:", 2010, 296, "Greg", "9788373271890");
-		Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
+		final String appName = "Biblioteka v0.6";
+		
+		Book book[]=new Book[1000];
+		book[1] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz:", 2010, 296, "Greg", "9788373271890");
+		book[2] = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
 				"9788324620845");
-		Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
+		book[3] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
 				2008, 851, "McGraw-Hill Osborne Media", "9780071591065");
 		System.out.println(appName);
 		System.out.println("Ksi¹¿ki w bibliotece: ");
-		book1.printInfo();
-		book2.printInfo();
-		book3.printInfo();
+		book[1].printInfo();
+		book[2].printInfo();
+		book[3].printInfo();
 	}
 
 }
